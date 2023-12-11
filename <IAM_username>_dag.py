d7815608a8f2c6ba3276c11ae9b3338b09f23c2c
kimiko <kinqsleyy@gmail.com>
2023-12-11 19:20:21 +0000

    added the DAG file

---
+++ <IAM_username>_dag.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/kinqsleyy@gmail.com/batch_processing',
+}
+
+default_args = {
+    'owner': 'kimiko',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+#Creating the DAG.
+with DAG('0aedb74d3ba3_dag',
+    start_date=datetime.now(),
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
